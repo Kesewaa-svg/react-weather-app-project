194b016546612988a850f10ee43a9021da75012d
Kesewaa-svg <146530240+Kesewaa-svg@users.noreply.github.com>
2024-01-21 15:02:36 +0100

    Added the basic CSS styling

--- src/Weather.js
+++ src/Weather.js
@@ -6,7 +6,7 @@ export default function Weather() {
     <div className="Weather">
       <form>
         <div className="row">
-          <div className="col-9">
+          <div className="col-8">
             <input
               type="search"
               placeholder="Type a city..🙂"
@@ -14,12 +14,12 @@ export default function Weather() {
               autoFocus="on"
             />
           </div>
-          <div className="col-3">
+          <div className="col-4">
             <input
               type="submit"
               value="Search"
-              className="btn btn-success"
-              w-100
+              className="btn btn-success
+              w-100"
             />
           </div>
         </div>
@@ -29,14 +29,17 @@ export default function Weather() {
         <li>Sunday 15:00</li>
         <li>Snow</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://lh3.googleusercontent.com/k0gAYlLrKnJ4Oxtwbs1XRM5cnjXgiUMXx9ClMugpC34_4D45DRTExwUPHa0UpUo4lRy0j1BLPgoK8EV6LkKFvdzQWA=s60"
-            alt="Snow"
-          />{" "}
-          <span className="temperature">-1</span>{" "}
-          <span className="unit">°C</span>
+          <div className="clearfix">
+            <img
+              src="https://lh3.googleusercontent.com/k0gAYlLrKnJ4Oxtwbs1XRM5cnjXgiUMXx9ClMugpC34_4D45DRTExwUPHa0UpUo4lRy0j1BLPgoK8EV6LkKFvdzQWA=s60"
+              alt="Snow"
+              className="float-left"
+            />
+            <span className="temperature">-1</span>{" "}
+            <span className="unit">°C</span>
+          </div>
         </div>
         <div className="col-6">
           <ul>
