b0d0323cd17323687901e582a678e66a3d73befa
Kesewaa-svg <146530240+Kesewaa-svg@users.noreply.github.com>
2024-01-21 13:51:29 +0100

    Added GitHub repository

--- src/App.js
+++ src/App.js
@@ -4,9 +4,25 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Weather App</p>
-      </header>
+      <p>Weather App</p>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://jessica-abiri-portfolio.netlify.app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Jessica Abiri
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/Kesewaa-svg/react-weather-app-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
