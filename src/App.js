fdfa04ec7b505649e5d25779c52cc306f8a54c51
Kesewaa-svg <146530240+Kesewaa-svg@users.noreply.github.com>
2024-01-21 13:57:09 +0100

    Added Bootstrap to the application

--- src/App.js
+++ src/App.js
@@ -4,25 +4,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <p>Weather App</p>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://jessica-abiri-portfolio.netlify.app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Jessica Abiri
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/Kesewaa-svg/react-weather-app-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://jessica-abiri-portfolio.netlify.app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Jessica Abiri
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Kesewaa-svg/react-weather-app-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
